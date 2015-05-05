bc6f69af306626dae6aeb85a37cee631fcaa168f
FelipeMSX <felipemsx18@gmail.com>
2015-05-05 20:09:09 -0300

    Fazendo Quicksort

--- src/PAA/QuickSort.java
+++ src/PAA/QuickSort.java
@@ -14,11 +14,10 @@ import java.io.IOException;
 public class QuickSort {
 
     static StringBuilder steps = new StringBuilder();// Armazena toda os passos que serão gravados no arquivo de saída.
-    static FileData[] ConteinersSaida; // Vetor que armazena os registros armazenado.
-
+    static short stepsPadrao;
+    static short stepsMediana;
     public static void main(String[] args) throws IOException
     {
-
         if(args.length !=0)
         {
             loadFileData(args[0]);
@@ -49,12 +48,18 @@ public class QuickSort {
         BufferedReader br = new BufferedReader(fr);
         try {
             //Lê os contêiners cadastrados
-            int fileSize = Integer.parseInt(br.readLine());
-            FileData[] ConteinersCadastrados = new FileData[fileSize];
-            int count = 0;
-            while (br.ready() && count < fileSize) {
-                ConteinersCadastrados[count] = examineLine(br.readLine());
-                count++;
+            int qtdVectors= Integer.parseInt(br.readLine());
+            while (br.ready()) {
+                int qtdNumbers = Integer.parseInt(br.readLine());
+                int vector[] = new int[qtdNumbers];
+                examineLine(vector,br.readLine());
+                /*
+                stepsPadrao =0;
+                quicksortPadrao(vector, 0, vector.length - 1);
+                */
+                stepsMediana = 0;
+                quicksortMediana(vector,0,vector.length-1);
+                System.out.println(stepsMediana);
             }
 
             br.close();
@@ -64,15 +69,21 @@ public class QuickSort {
         }
     }
 
-    public static FileData examineLine(String line){
-        FileData data 		= new FileData();
+    public static void swapItem(int[] input, int positionX, int positionY)
+    {
+        int temp = input[positionX];
+        input[positionX] = input[positionY];
+        input[positionY] = temp;
+    }
+
+    public static void examineLine(int[] vector,String line){
+
         String[] analise 	= line.split(" ");
-        data.ID 			= analise[0];
-        data.CNPJ 			= analise[1];
-        data.weight = Integer.parseInt(analise[2]);
+        for (int i = 0; i < analise.length; i++)
+        {
+        vector[i] = Integer.parseInt(analise[i]);
 
-        return data;
-
+        }
     }
 
     // Responsável por criar cada passo da função.
@@ -81,22 +92,76 @@ public class QuickSort {
 
     }
 
-    //PP : particionar padrão
-    public static void particionarPadrao()
-    {
+    private static void quicksortPadrao(int input[], int inicio, int fim) {
+        stepsPadrao++;// Número de chamadas
+        if(inicio < fim) {
+            int pivo = particionarPadrao(input, inicio, fim);
+            quicksortPadrao(input, inicio, pivo - 1);
+            quicksortPadrao(input, pivo + 1, fim);
 
+        }
+    }
+
+
+    //PP : particionar padrão
+    public static int particionarPadrao(int input[], int init, int end)
+    {
+        int pivo = input[end]; // pivô escolhido no final
+        int i = init - 1, j;
+        for(j = init; j < end; j++) {
+            if(input[j] <= pivo) {
+                i = i + 1;
+                stepsPadrao++;
+                swapItem(input,i,j);
+            }
+        }
+        stepsPadrao++;
+        swapItem(input,i+1,end);
+        return i + 1;
+    }
+
+
+    private static void quicksortMediana(int input[], int inicio, int fim) {
+        stepsMediana++;// Número de chamadas
+        if(inicio < fim) {
+            int pivo = particionarMediana(input, inicio, fim);
+            quicksortMediana(input, inicio, pivo - 1);
+            quicksortMediana(input, pivo + 1, fim);
+
+        }
     }
 
     //PM particionar por mediana de 3
-    public static void particionarMediana()
+    public static int particionarMediana(int input[], int init, int end)
     {
-
+        int pivo = calcPivoMediana(input,(end-init)) ;
+        int i = init - 1, j;
+        for(j = init; j < end; j++) {
+            if(input[j] <= pivo) {
+                i = i + 1;
+                stepsMediana++;
+                swapItem(input,i,j);
+            }
+        }
+        stepsMediana++;
+        swapItem(input,i+1,end);
+        return i + 1;
     }
 
     //PA particionar por pivô aleatório
     public static void particionarPivoAleatorio()
     {
 
+    }
+
+    private static void quicksortHoarePadrao(int input[], int inicio, int fim) {
+        stepsMediana++;// Número de chamadas
+        if(inicio < fim) {
+            int pivo = particionarMediana(input, inicio, fim);
+            quicksortHoarePadrao(input, inicio, pivo - 1);
+            quicksortHoarePadrao(input, pivo + 1, fim);
+
+        }
     }
 
     //HP hoare padrão
@@ -117,13 +182,24 @@ public class QuickSort {
 
     }
 
-    public static class FileData
+
+    public static int calcPivoMediana(int vector[], int n)
     {
-        private String ID;
-        private String CNPJ;
-        private int weight;
-        private short mergePriority;
-        private String output; // armazena o conteúdo a ser gerado no arquivo de saída.
+
+        int v1 = vector[n/4];
+        int v2 = vector[n/2];
+        int v3 = vector[3*n/4];
+        int[] temp = new int[3];
+        temp[0] = v1;
+        temp[1] = v2;
+        temp[2] = v3;
+        quicksortPadrao(temp, 0, 2);
+        return ( temp[1]);
+    }
+
+    public static int calcPivoAleatorio(int vector[], int init)
+    {
+        return vector[init + Math.abs(vector[init]) % vector.length ];
     }
 
 }

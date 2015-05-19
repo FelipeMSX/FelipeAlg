4b6c2c9be19645b6452db95042953a263491406e
felipemsx <felipemsx18@gmail.com>
2015-05-19 11:05:46 -0300

    Melhoria no Merge

--- src/PAA/MergeSort.java
+++ src/PAA/MergeSort.java
@@ -69,7 +69,7 @@ public class MergeSort {
 
                 //Lista de Contêiners com erros
                 int fileSize2 = Integer.parseInt(br.readLine());
-                int controladorF = fileSize-1;
+                int controladorF = fileSize;
                 ConteinersOrdenar = new FileData[fileSize2];
                 while (br.ready())
                 {
@@ -81,8 +81,7 @@ public class MergeSort {
                         if(data.ID.equals(ConteinersCadastrados[i].ID))
                         {
                             dataAchado = ConteinersCadastrados[i];
-                            ConteinersCadastrados[i] = ConteinersCadastrados[controladorF];
-                            controladorF--;
+                            ConteinersCadastrados[i] = ConteinersCadastrados[--controladorF];
                             i = controladorF;
 
                         }
@@ -105,7 +104,6 @@ public class MergeSort {
                         }
                     }
                 }
-                System.out.println("Fim");
                 br.close();
                 fr.close();
             } catch (IOException e) {

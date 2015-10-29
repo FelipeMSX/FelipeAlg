802db8738c0e225ae139863b6877db66d87b081f
FelipeMSX <felipemsx18@gmail.com>
2015-10-28 21:56:29 -0300

    Adicinado KMP 26/10

--- src/PAA/KMP.java
+++ src/PAA/KMP.java
@@ -13,24 +13,35 @@ public class KMP {
 	static int diseaseCount;                            // Total de doenças.
 	static FileData[] Diseases;                         // Armazena o total de doenças à pesquisar.
 	static StringBuilder steps = new StringBuilder();   // Armazena toda os passos que serão gravados no arquivo de saída.
+	static int lastPositionValid;
+
 	public static void main(String args[]) throws IOException
 	{
-		if(args.length !=0) {
-			readInputFile(args[0]);
-			runKMP();
-			writeSteps(args[0]);
-		}
-
-
-	KMPTable table = calcTableKMP("ara");
-	int x = 0;
-		System.out.println(table);
-
+//		if(args.length !=0) {
+//			readInputFile(args[0]);
+//			runKMP();
+//			writeSteps(args[0]);
+//		}
+		validGeneCountMin = 3;
+		KMPTable table =  calcTableKMP("AATAAT");
+		int qtdMatched = searchKMP("AAAATTTCGTTAAATTTGAACATAGGGATA",table);
+		System.out.println(qtdMatched);
 	}
 
 	public static void runKMP()
 	{
+		int diseaseCount = 0;
+		while(Diseases.length != diseaseCount)
+		{
+			FileData disease = Diseases[diseaseCount];
 
+			//Para cada gene fazer a pesquisa na sequência genética inteira.
+			for(int i = 0; i < disease.gene.length; i++)
+			{
+				calcTableKMP(disease.gene[i]);
+			}
+			diseaseCount++;
+		}
 	}
 
 	public static KMPTable calcTableKMP(String input) {
@@ -61,12 +72,66 @@ public class KMP {
 		return table;
 	}
 
-	public void searchKMP(String text, KMPTable table)
+	//retorna a posição da ocorrência do primeiro match no texto;
+	public static int searchKMP(String text, KMPTable table)
 	{
+		int tableP = -1;//TablePonteiro
+		int textP = 0 ;///TextoPonteiro
+		int textLength = text.length();
+		int	tableLength = table.charSequence.length();
 
+		lastPositionValid = -1;
+		int tempGenCount = 0;
+		//Texto menor que o padrão
+		if(tableLength > textLength)
+			return -1;
+
+		StringBuilder sTeste = new StringBuilder();
+		while(textP != textLength)
+		{
+			sTeste.append(text.charAt(textP));
+			if(text.charAt(textP) == table.charSequence.charAt(tableP+1)) {
+				tableP++;
+				tempGenCount++;
+
+				//FullMatch
+				if(tableP+1 == tableLength)
+				{
+					if(tempGenCount >= validGeneCountMin) {
+						lastPositionValid = tableP;
+					}
+					return ++lastPositionValid;
+				}
+
+			}else{
+				if(tempGenCount >= validGeneCountMin) {
+					lastPositionValid = tableP;
+					int t = (tableLength - (tableP+1));
+					//CanBreak
+					if( (tableLength - (tableP+1)) < validGeneCountMin)
+					{
+						return ++lastPositionValid;
+					}
+				}
+				tempGenCount = 0;
+				//Se for diferente atribuir o valor da tabela calculado correspondente
+				tableP = tableP == -1 ? -1 :table.bigPS[tableP];
+
+				//Corrigir caso fique antes do lastPositionValid
+				if(tableP < lastPositionValid)
+					tableP = lastPositionValid;
+
+				int calc = lastPositionValid;
+				while(calc < tableP) {
+				calc++;
+						tempGenCount++;
+				}
+			}
+				textP++;
+
+		}
+		return ++lastPositionValid; //nomatchFound
 	}
-
-
 
 	public static void writeSteps(String filePath) throws IOException
 	{
@@ -95,7 +160,7 @@ public class KMP {
 				validGeneCountMin   = Integer.parseInt(br.readLine());
 				geneticSequence     = br.readLine();
 				diseaseCount        = Integer.parseInt(br.readLine());
-				FileData[] Diseases = new FileData[diseaseCount];
+				Diseases = new FileData[diseaseCount];
 				int count = 0;
 				while (br.ready())
 				{
